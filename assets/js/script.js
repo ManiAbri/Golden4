30bd4cfc32ac8e613d9e837e8ff0354398642644
ManiAbri <abrimani0@gmail.com>
2024-10-08 12:28:57 -0700

    update mainpage + js animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,31 @@ $(document).ready(function () {
         checkWidth()
     })
 })
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById('preloader').style.animation = 'flyIn 1s forwards';
+        setTimeout(function() {
+            document.getElementById('preloader').style.display = 'none';
+            document.getElementById('content').style.display = 'block';
+        }, 1000); // زمان انیمیشن flyIn
+    }, 3000); // زمان نمایش preloader
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const flyInSection = document.querySelector('.fly-in-section');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                flyInSection.classList.add('visible');
+            }
+        });
+    });
+
+    observer.observe(flyInSection);
+});
+
